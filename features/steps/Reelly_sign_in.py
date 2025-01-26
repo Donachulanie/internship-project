8b24093881abe3572dfdfe21b096c9baa727c5ab
Donachulanie <donachulanie@github.com>
2025-01-26 16:28:21 -0500

    Internship Project Task 1

---
+++ features/steps/Reelly_sign_in.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from behave import given, when, then
+from time import sleep
+
+
+
+@given('Open Reelly main page')
+def open_main(context):
+    context.app.main_page.open_main()
+
+@when('input valid {email} and {password} on sign in page')
+def input_valid_email_and_password(context,email,password):
+    context.app.sign_in_page.input_valid_email_and_password(email,password)
+
+@then ('Click on the "Continue" button')
+def click_continue_button(context):
+    context.app.sign_in_page.click_continue_button()
+
+@then('Verify user is logged in successfully')
+def verify_user_logged_in(context):
+    context.app.sign_in_page.verify_user_logged_in()
---
+++ features/steps/reelly_main_page.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from behave import given, when, then
+from time import sleep
+
+@when('Click on “Off plan” at the left side menu')
+def click_on_off_plan_from_left_side_menu(context):
+    context.app.header.click_on_off_plan_from_side_menu()
+
+
+@then('Verify Presale(EOI) box shown')
+def verify_presale_eoi(context):
+    context.app.header.verify_presale_eoi()
+
+
+@when('Filter by sale status of “Out of Stock”')
+def filter_by_sale_status_of_out_of_stock(context):
+    context.app.header.filter_by_sale_status_of_out_of_stock()
+
+@then('Verify each product contains the Out of Stock')
+def verify_out_of_stock_box(context):
+    context.app.header.verify_out_of_stock_box()
